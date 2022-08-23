a24816d9df562c518aee69f2753fefd6c71db9cb
Mayur_Chauhan <mayurchauhan@live.com>
2022-08-23 16:24:22 +0100

    Reaction Porject

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Mayur Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResult" :score="score" />
+</template>
+
+<script>
+// components
+import Block from "./components/Block.vue";
+import Result from "./components/Result";
+
+export default {
+  name: "App",
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,24 @@
+<template>
+  <h4>Reaction Time: {{ score }} MS</h4>
+  <p>You have {{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail Pace...";
+    }
+  },
+};
+</script>
